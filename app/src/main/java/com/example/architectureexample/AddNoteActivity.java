497b147d904dd2814d2b84f4a2c34fa559c69a98
sulaimanashiru <dash.ashiru@gmail.com>
2020-01-21 13:52:30 -0500

    added drawables, activity_add_note, and add_note_menu, AddNoteActivity

---
+++ app/src/main/java/com/example/architectureexample/AddNoteActivity.java
@@ -0,0 +1,97 @@
+package com.example.architectureexample;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    public static final String  EXTRA_TITLE =
+            "com.example.architectureexample.EXTRA_TITLE";
+
+    public static final String  EXTRA_DESCRIPTION =
+            "com.example.architectureexample.EXTRA_DESCRIPTION";
+
+    public static final String  EXTRA_PRIORITY =
+            "com.example.architectureexample.EXTRA_PRIORITY";
+
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private NumberPicker numberPickerPriority;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+        numberPickerPriority = findViewById(R.id.number_picker_priority);
+
+        numberPickerPriority.setMinValue(1);
+        numberPickerPriority.setMaxValue(10);
+
+        getActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+        setTitle("Add Note");
+
+    }
+
+    private void saveNote(){
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        int priority = numberPickerPriority.getValue();
+
+        if(title.trim().isEmpty() || description.trim().isEmpty()) {
+            Toast.makeText(this,"Please insert a title and description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent data = new Intent ();
+        data.putExtra(EXTRA_TITLE,description);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        data.putExtra(EXTRA_PRIORITY, priority);
+
+        setResult(RESULT_OK, data);
+        finish();
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
+    }
+}
+
